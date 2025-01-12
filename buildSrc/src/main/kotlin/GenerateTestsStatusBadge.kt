15cd682f3a625d8f3d572de7f05b2a5229665cae
Sztorm <marcinwicher99@gmail.com>
2025-01-12 21:01:09 +0100

    Updated generateTestStatusBadge task.

--- buildSrc/src/main/kotlin/GenerateTestsStatusBadge.kt
+++ buildSrc/src/main/kotlin/GenerateTestsStatusBadge.kt
@@ -9,50 +9,55 @@ abstract class GenerateTestsStatusBadge : DefaultTask() {
     @get:OutputFile
     abstract val badgeOutput: RegularFileProperty
 
-    private fun getTestsStatusResult(reportFile: String): String {
-        val regex = Regex("""<div class="infoBox" id="failures">\s+<div class="counter">(\d+)</div>""")
+    private fun getTestsStatusResult(reportFile: String): StatusResult {
+        val regex =
+            Regex("""<div class="infoBox" id="failures">\s+<div class="counter">(\d+)</div>""")
         val match = regex.find(reportFile)
 
         return when {
-            match == null -> "Failing"
+            match == null -> StatusResult.Unknown
             else -> {
                 val failures = match.groupValues[1].toInt()
-                println(match)
-                println(failures)
                 when (failures) {
-                    0 -> "Passing"
-                    else -> "Failing"
+                    0 -> StatusResult.Passing
+                    else -> StatusResult.Failing
                 }
             }
         }
     }
 
     @Suppress("SpellCheckingInspection")
-    private fun getTestsStatusBadge(statusResult: String): String {
-        val statusColor = when (statusResult) {
-            "Passing" -> "rgb(68, 204, 17)"
-            else -> "rgb(199, 79, 60)"
+    private fun getTestsStatusBadge(statusResult: StatusResult): String {
+        val badgeColor = when (statusResult) {
+            StatusResult.Passing -> passingColor.formatRgb()
+            StatusResult.Failing -> failingColor.formatRgb()
+            StatusResult.Unknown -> unknownColor.formatRgb()
+        }
+        val formattedStatus = when (statusResult) {
+            StatusResult.Passing -> "Passing"
+            StatusResult.Failing -> "Failing"
+            StatusResult.Unknown -> "Unknown"
         }
         val result =
             """
-            <svg xmlns="http://www.w3.org/2000/svg" width="87" height="20">
+            <svg xmlns="http://www.w3.org/2000/svg" width="90" height="20">
                 <linearGradient id="smooth" x2="0" y2="100%">
                     <stop offset="0" stop-color="#bbb" stop-opacity=".1"/>
                     <stop offset="1" stop-opacity=".1"/>
                 </linearGradient>
                 <mask id="round">
-                    <rect width="87" height="20" rx="3" fill="#fff"/>
+                    <rect width="90" height="20" rx="3" fill="#fff"/>
                 </mask>
                 <g mask="url(#round)">
-                    <rect width="35" height="20" fill="#555"/>
-                    <rect width="52" height="20" x="35" fill="$statusColor"/>
-                    <rect width="87" height="20" fill="url(#smooth)"/>
+                    <rect width="38" height="20" fill="#555"/>
+                    <rect width="52" height="20" x="38" fill="$badgeColor"/>
+                    <rect width="90" height="20" fill="url(#smooth)"/>
                 </g>
                 <g fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="11">
-                    <text x="17" y="15" fill="#010101" fill-opacity=".3">Tests</text>
-                    <text x="17" y="14">Tests</text>
-                    <text x="61" y="15" fill="#010101" fill-opacity=".3">$statusResult</text>
-                    <text x="61" y="14">$statusResult</text>
+                    <text x="19" y="15" fill="#010101" fill-opacity=".3">Tests</text>
+                    <text x="19" y="14">Tests</text>
+                    <text x="64" y="15" fill="#010101" fill-opacity=".3">$formattedStatus</text>
+                    <text x="64" y="14">$formattedStatus</text>
                 </g>
             </svg>
             """.trimIndent()
@@ -65,8 +70,21 @@ abstract class GenerateTestsStatusBadge : DefaultTask() {
         val testsStatusBadge = testsStatusReportInput.get().asFile
             .readText()
             .let(::getTestsStatusResult)
+            //.let { StatusResult.Unknown }
             .let(::getTestsStatusBadge)
 
         badgeOutput.get().asFile.writeText(testsStatusBadge)
+    }
+
+    sealed class StatusResult {
+        object Passing : StatusResult()
+        object Failing : StatusResult()
+        object Unknown : StatusResult()
+    }
+
+    companion object {
+        private val passingColor = Color(68u, 204u, 17u, 255u)
+        private val failingColor = Color(199u, 79u, 60u, 255u)
+        private val unknownColor = Color(155u, 155u, 155u, 255u)
     }
 }
